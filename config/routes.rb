0d18c90ef101c1401ce9d8af8b647bbae7b1e804
Fábio Tanaka <fabioqtanaka@hotmail.com>
2017-08-27 18:38:57 -0300

    Bootstrap integrado e adicição de 2 templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'uipages/user_ui'
+
+  get 'uipages/employee_ui'
+
+  get 'pages/home'
+  get 'pages/main_ui'
+  get 'pages/sobre'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 end
