6205a50017177739524659a47b33563da7452453
MrCasimiro <guircasimiro@gmail.com>
2017-09-19 05:49:47 -0300

    fix login, update some tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-	def get_css_path
-		if @css_file.nil?
-			@css_file = "/css/landing-page.css"
-		else
-			@css_file
-		end
-	end
-
-	def set_css_path(file)
-		@css_file = file
-	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,4 @@
-module SessionsHelper
+	module SessionsHelper
 
 	def log_in(user)
 		session[:user_id] = user.id
@@ -6,10 +6,15 @@ module SessionsHelper
 
 	def current_user
 		# X ||=Y => X = X || Y
-		@current_user ||= User.find_by(id: session[:user_id]) 
+		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
 	def logged_in?
 		!current_user.nil?
+	end
+
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
 	end
 end
