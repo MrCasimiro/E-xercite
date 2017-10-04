c44f67a952d8edaa20d78d021e82597866a31e0f
AmandaMMFunabashi <amandafunabashi@gmail.com>
2017-10-04 11:06:20 -0300

    seeding

---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+	def show
+	  	@coach = Coach.find(params[:id])
+  		# debugger # opens a rails console in prompt to debug
+  end
+
+	def index
+		@coach = Coach.find(current_person.id)
+		redirect_to @coach
+	end
+	
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = Person.create(name:'user1test', email:'user1test@test.com', phone:'00000000', age: 20, gender: 'm', password:'abcd1234', password_confirmation:'abcd1234')
+u2 = Person.create(name:'user2test', email:'user2test@test.com', phone:'00000000', age: 20, gender: 'm', password:'abcd1234', password_confirmation:'abcd1234')
+c = Person.create(name:'coachtest', email:'coachtest@test.com', phone:'00000000', age: 20, gender: 'm', password:'abcd1234', password_confirmation:'abcd1234')
+a = Person.create(name:'admintest', email:'admintest@test.com', phone:'00000000', age: 20, gender: 'm', password:'abcd1234', password_confirmation:'abcd1234')
+
+u1.create_user(level: 0, points: 0)
+u2.create_user(level: 0, points: 0)
+c.create_coach()
+a.create_admin(adm_password: '00000000')
+
