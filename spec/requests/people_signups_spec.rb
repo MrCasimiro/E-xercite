9f6ea880fec06abcba5b419d9be1a0ee6596ab12
MrCasimiro <guircasimiro@gmail.com>
2017-10-03 21:22:04 -0300

    fix specs errors

--- spec/requests/people_signups_spec.rb
+++ spec/requests/people_signups_spec.rb
@@ -1,27 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "UsersSignups", type: :request do
+RSpec.describe "PeopleSignups", type: :request do
   describe "post /signup" do
     it "invalid signup information" do
 	    get signup_path
 
 	    expect do
 		 	post signup_path, params: {
-		   		user: { name: "",
+		   		person: { name: "",
 		   				email: "user@invalid",
 	     				age: 1234,
 	      				phone: "",
 	      				gender: "",
-	      				lesionHistory: "",
-	      				diseases: "",
-	      				restrictions: "",
 	      				password: "1234",
     					password_confirmation: "1234"
 	      			}
 	      	}
-	    end.to_not change{ User.count }
+	    end.to_not change{ Person.count }
 
-	    expect(response).to render_template('users/new')
+	    expect(response).to render_template('people/new')
 	    #assert_select 'div#<CSS id for error explanation>'
 	   	#assert_select 'div.<CSS class for field with error>'
 
@@ -34,19 +31,16 @@ RSpec.describe "UsersSignups", type: :request do
 
     	expect do
     		post signup_path, params: {
-    			user: { name: "Exemplo",
+    			person: { name: "Exemplo",
     					email: "exemplo@exemplo.com",
     					age: 12,
     					phone: "1111111",
     					gender: "other",
-    					lesionHistory: "none",
-    					diseases: "none",
-    					restrictions: "none",
     					password: "123456",
     					password_confirmation: "123456"
     				}
     		}
-    	end.to change {User.count}	
+    	end.to change {Person.count}	
     	follow_redirect!
     	expect(response).to render_template("users/show")
     	expect(flash).to_not be_empty
