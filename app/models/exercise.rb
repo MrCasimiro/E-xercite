e8b1cedeb87b4f2951b00a1fb18fe748ab68cc0a
MrCasimiro <guircasimiro@gmail.com>
2017-10-04 20:09:50 -0300

    fix issue on database

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,4 @@
 class Exercise < ApplicationRecord
+	has_many :workout_composes
+	has_many :workouts, through: :workout_composes 
 end
--- db/migrate/20170922030309_create_users.rb
+++ db/migrate/20170922030309_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
+  	drop_table :users
     create_table :users do |t|
       t.belongs_to :person, index: { unique: true}, foreign_key:true
       t.integer :level
