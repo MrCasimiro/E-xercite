240be4a78cdedf9ffc3675e05b9621db2ac36fa3
Juny <jun3120@gmail.com>
2017-10-07 13:01:16 -0300

    Fix exercise controller

--- spec/controllers/exercises_controller_spec.rb
+++ spec/controllers/exercises_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ExerciseController, type: :controller do
+RSpec.describe ExercisesController, type: :controller do
 
 	describe "GET #new" do
 		it "returns http success" do
