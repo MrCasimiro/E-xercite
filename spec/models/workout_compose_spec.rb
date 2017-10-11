c95aefef61e66e37be94ff7158783aead80990c5
Juny <jun3120@gmail.com>
2017-10-11 01:29:40 -0300

    Update workout_compose_spec.rb

--- spec/models/workout_compose_spec.rb
+++ spec/models/workout_compose_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe WorkoutCompose, type: :model do
   before :each do
 		exercise1 = Exercise.create(name_exercise: "Flexao")
-		person3 = Person.create!(name: "coach", email: "coach@coach.com",
- 	  phone: "1111111", age: 21, gender: "other", password: "123456", password_confirmation: "123456")
 
 		coach = Coach.create!(person_id: person3.id)
 		workout1 = Workout.create(coach_id: coach.id)
