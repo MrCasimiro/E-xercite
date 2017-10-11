8a78fcba7996321b107b607854025062fa92333b
Juny <jun3120@gmail.com>
2017-10-11 01:16:29 -0300

    Update workout_spec.rb

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Workout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+		person3 = Person.create!(name: "coach", email: "coach@coach.com",
+ 	  phone: "1111111", age: 21, gender: "other", password: "123456", password_confirmation: "123456")
+
+    coach = Coach.create!(person_id: person3.id)
+    @workout = Workout.new(coach_id: coach.id)
+    
+  end
+    
+  it "should be pass" do
+    expect(@workout).to be_valid
+  end
+
+
+  it "coach_id should be present" do
+    @workout.coach_id = nil
+    expect(@workout).to_not be_valid
+  end
+    
 end
