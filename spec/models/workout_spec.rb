629e4a7e295171fd494a87dcce95fbb64e4cbd21
Juny <jun3120@gmail.com>
2017-10-11 01:29:59 -0300

    Update workout_spec.rb

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Workout, type: :model do
   before :each do
-		person3 = Person.create!(name: "coach", email: "coach@coach.com",
- 	  phone: "1111111", age: 21, gender: "other", password: "123456", password_confirmation: "123456")
 
     coach = Coach.create!(person_id: person3.id)
     @workout = Workout.new(coach_id: coach.id)
