e25b99494ec62e238e8d59681a5c61586820d408
Juny <jun3120@gmail.com>
2017-10-18 18:50:12 -0200

    Update upload_backups_spec.rb

--- spec/jobs/upload_backups_spec.rb
+++ spec/jobs/upload_backups_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe UploadBackupsJob, :type => :job do
+RSpec.describe ApplicationJob, :type => :job do
   describe "#perform_later" do
     it "uploads a backup" do
       ActiveJob::Base.queue_adapter = :test
