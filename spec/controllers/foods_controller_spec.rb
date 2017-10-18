8dd4502905ebd8eef42a699cc72e9ad3cbce346e
Juny <jun3120@gmail.com>
2017-10-18 18:17:14 -0200

    Update foods_controller_spec.rb

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe FoodsController, type: :controller do
+  describe "GET #new" do
+     it "expect a new workout" do
+      get :new
+       expect(assigns(:food)).to be_a_new(Food)
+    end
+  end
 
 end
