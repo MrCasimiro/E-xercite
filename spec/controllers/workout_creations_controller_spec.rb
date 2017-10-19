573a03d6549173a19554ecb463ef00f170196a0b
AmandaMMFunabashi <amandafunabashi@gmail.com>
2017-10-19 17:14:08 -0200

    adjusting tests

--- spec/controllers/workout_creations_controller_spec.rb
+++ spec/controllers/workout_creations_controller_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe WorkoutCreationsController, type: :controller do
 	    it "returns http success" do 
 	    	person = Person.create!(name: "coach", email: "coach3@coach.com",
  					phone: "1111111", age: 21, gender: "other", password: "123456", password_confirmation: "123456")
-	    	get :show, params: {id: Coach.create(person_id: person.id)}
+	    	person2 = Person.create!(name: "user", email: "user3@user.com",
+ 					phone: "1111111", age: 21, gender: "other", password: "123456", password_confirmation: "123456")
+	    	get :show, params: {id: Coach.create(person_id: person.id), id_user: User.create(person_id: person2.id)}
 	    	expect(response).to have_http_status(:success)
 	    end
   end
