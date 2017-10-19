a3008e46fb2e7844aca96c964881416bfd275ef7
Juny <jun3120@gmail.com>
2017-10-19 01:40:26 -0200

    Update channel.rb

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -3,7 +3,7 @@ module ApplicationCable
   def say_hello(data)
     times_to_say_hello = data.fetch("times_to_say_hello")
     hello = "Hello, #{current_profile.name}!"
-
+  end
     times_to_say_hello.times do
       ActionCable.server.broadcast(current_profile.id, hello)
     end
