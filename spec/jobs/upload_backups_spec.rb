491ae2a7a57aed045878a0fb9564e74d1cbc4677
Juny <jun3120@gmail.com>
2017-10-19 00:14:04 -0200

    Update upload_backups_spec.rb

--- spec/jobs/upload_backups_spec.rb
+++ spec/jobs/upload_backups_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ApplicationJob, :type => :job do
     it "uploads a backup" do
       ActiveJob::Base.queue_adapter = :test
       expect {
-        UploadBackupsJob.perform_later('backup')
+        Application.perform_later('backup')
       }.to have_enqueued_job
     end
   end
