e7f68f68138e0bb0258a496fd8d7c9741d63ef88
Jun Yamada <jun3120@gmail.com>
2017-10-25 01:26:36 -0200

    css fix

--- app/controllers/workout_menu_controller.rb
+++ app/controllers/workout_menu_controller.rb
@@ -2,7 +2,6 @@ class WorkoutMenuController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@active_workout_search = UserDoWorkout.where(
-			'user_id = :id and finished = 0 and exp_date >= :today',
 			:id => @user.id,
 			:today => Time.now
 			)
@@ -13,12 +12,5 @@ class WorkoutMenuController < ApplicationController
 			@exercise_list = WorkoutCompose.where('workout_id = :id',
 			:id => @active_workout.workout_id)
 		end
-
-
-		@old_workouts = UserDoWorkout.where(
-			'user_id = :id and (finished <> 0 or exp_date < :today)',
-			:id => @user.id,
-			:today => Time.now
-			)
 	end
 end
