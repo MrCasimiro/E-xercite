9b4194b305a04d8cc02978f5eb6cd22eb20c391b
Fábio Tanaka <fabioqtanaka@hotmail.com>
2017-11-06 23:02:20 -0200

    Merged

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030105104) do
-
+ActiveRecord::Schema.define(version: 20171106205402) do
 
   create_table "admins", force: :cascade do |t|
     t.string "adm_password"
