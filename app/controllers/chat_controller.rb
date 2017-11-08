8b6babce44152e31371d15bd986c1815280b660c
Jun Yamada <jun@looqbox.com>
2017-11-08 17:19:10 -0200

    update chat

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,7 +2,7 @@ class ChatController < ApplicationController
   def index
     session[:conversations] ||= []
 
-    @person = Person.all.where.not(id: current_person)
+    @people = Person.all.where.not(id: current_person)
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
   end
