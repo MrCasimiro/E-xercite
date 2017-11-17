69afe2ad6a63ff3ebb5c9c672fc4265ce5069354
Juny <jun3120@gmail.com>
2017-11-17 11:30:02 -0200

    Update sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,10 @@
 module SessionsHelper
 	
     def current_user
-		@current_user ||= User.find_by(person_id: current_person.id)
-	end
+	@current_user ||= User.find_by(person_id: current_person.id)
+    end
 
-	def current_coach
-		@current_coach ||= Coach.find_by(person_id: current_person.id)
-	end
+    def current_coach
+	@current_coach ||= Coach.find_by(person_id: current_person.id)
+    end
 end
