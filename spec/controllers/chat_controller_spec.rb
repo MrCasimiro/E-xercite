85ee4c2cd49843785be30ba65589bb6d1268c2b2
juny <jun3120@gmail.com>
2017-11-24 00:45:29 -0200

    socket

--- spec/controllers/chat_controller_spec.rb
+++ spec/controllers/chat_controller_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ChatController, type: :controller do
+	before :each do
+		@person = FactoryGirl.create(:person)
+		@conversation = FactoryGirl.create(:conversation)
+	end
 end
