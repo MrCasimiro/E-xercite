8bb223550f4f69a2343184ae8f71be2bc403c6aa
MrCasimiro <guircasimiro@gmail.com>
2017-11-27 19:24:43 -0200

    fix travis issue

--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -2,24 +2,28 @@ require 'rails_helper'
 
 RSpec.describe WorkoutsController, type: :controller do
 	before :each do 
+		@person = FactoryGirl.create(:person)
 		@user = FactoryGirl.create(:user)
-		@coach = FactoryGirl.create(:coach)
+		@coach = FactoryGirl.create(:coach, person_id: @person.id)
 		@workout = FactoryGirl.create(:workout)
 		@workout_compose = FactoryGirl.create(:workout_compose)
 	end
 
 	describe "GET #action" do
 		it "returns http success when show" do 
+			sign_in @person
 			get :workouts_page, params: {coach_id: @coach.id, id_user: @user.id, id: 1}
 			expect(response).to have_http_status(:success)
 		end
 
 		it "return http success when new" do
+			sign_in @person
 			get :new, params: {coach_id: @coach.id, id_user: @user.id, id: 1}
 			expect(response).to have_http_status(:success)
 		end
 
 		it "return http success when create a workout" do
+			sign_in @person
 			expect do
 				put :create, params: {
 					workout: {
@@ -37,6 +41,7 @@ RSpec.describe WorkoutsController, type: :controller do
 		end
 
 		it "return http success when send a workout to a user" do
+			sign_in @person
 			expect do
 				put :create_do_workout, params: {
 					coach_id: @coach.id,
@@ -47,6 +52,7 @@ RSpec.describe WorkoutsController, type: :controller do
 		end
 
 		it "return http success when view workouts history" do
+			sign_in @person
 			get :workouts_history, params: {coach_id: @coach.id}
 			expect(response).to have_http_status(:success)
 		end
