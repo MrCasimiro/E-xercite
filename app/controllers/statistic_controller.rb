22f9ffd27f7d871b425a272a7f8d41a522fa85a8
FelipeKusuki <felipesk26@gmail.com>
2017-12-03 20:43:00 -0200

    adicionando estatistica do usuario

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -1,9 +1,18 @@
 class StatisticController < ApplicationController
 	
-def show
-    #@user = User.find(params[:id])
-    @user = current_user
-  end
+	def show
+	    @user = User.find(params[:id])
+		@active_workout_search = UserDoWorkout.where(
+				:id => @user.id
+				)
+
+			@active_workout = @active_workout_search[0]
+
+			if @active_workout != nil then
+				@exercise_list = WorkoutCompose.where('workout_id = :id',
+				:id => @active_workout.workout_id)
+			end
+	end
 
 
 	
