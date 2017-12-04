04bafd58959328a23d606a68a69e01fc55eb9bec
Fábio Tanaka <fabioqtanaka@hotmail.com>
2017-12-04 01:34:16 -0200

    Novos dados user

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -8,5 +8,10 @@ class CoachesController < ApplicationController
 	def users_profile
 		@coach = Coach.find(params[:id])
 		@user_profile = User.find(params[:id_user])
+		@newest_info = UserMeasure.where(
+			'user_id = :id',
+			:id => @user_profile.id
+			).order("created_at").last
+		@newest_date = @newest_info.created_at.strftime('%d/%m/%y %H:%M')
 	end
 end
